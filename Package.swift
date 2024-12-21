640715a9c2253ff2dc082d1a9b4134dfe07886f6
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.nl>
2024-12-21 22:39:56 +0100

    compiling

--- Package.swift
+++ Package.swift
@@ -12,19 +12,10 @@ extension Target.Dependency {
 }
 
 extension Target.Dependency {
-    static var rateLimiter: Self { .product(name: "RateLimiter", package: "coenttb-utils") }
-    static var dependencies: Self { .product(name: "Dependencies", package: "swift-dependencies") }
-    static var dependenciesMacros: Self { .product(name: "DependenciesMacros", package: "swift-dependencies") }
-    static var language: Self { .product(name: "Languages", package: "swift-language") }
-    static var swiftHtml: Self { .product(name: "HTML", package: "swift-html") }
+    static var coenttbWeb: Self { .product(name: "CoenttbWeb", package: "coenttb-web") }
     static var codable: Self { .product(name: "MacroCodableKit", package: "macro-codable-kit") }
+    static var dependenciesMacros: Self { .product(name: "DependenciesMacros", package: "swift-dependencies") }
     static var memberwiseInit: Self { .product(name: "MemberwiseInit", package: "swift-memberwise-init-macro") }
-    static var coenttbHtml: Self { .product(name: "CoenttbHTML", package: "coenttb-html") }
-    static var environmentVariables: Self { .product(name: "EnvironmentVariables", package: "swift-environment-variables") }
-    static var tagged: Self { .product(name: "Tagged", package: "swift-tagged") }
-    static var urlRouting: Self { .product(name: "URLRouting", package: "swift-url-routing") }
-    static var urlFormCoding: Self { .product(name: "UrlFormCoding", package: "swift-web") }
-    static var swiftDate: Self { .product(name: "Date", package: "swift-date") }
 }
 
 let package = Package(
@@ -37,26 +28,17 @@ let package = Package(
         .library(name: .hotjar, targets: [.hotjar]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log", from: "1.5.0"),
-        .package(url: "https://github.com/coenttb/coenttb-html", branch: "main"),
-        .package(url: "https://github.com/coenttb/coenttb-utils.git", branch: "main"),
-        .package(url: "https://github.com/coenttb/swift-html", branch: "main"),
-        .package(url: "https://github.com/coenttb/swift-web", branch: "main"),
-        .package(url: "https://github.com/coenttb/swift-date", branch: "main"),
-        .package(url: "https://github.com/coenttb/swift-environment-variables.git", branch: "main"),
-        .package(url: "https://github.com/coenttb/swift-language.git", branch: "main"),
+        .package(url: "https://github.com/coenttb/coenttb-web", branch: "main"),
         .package(url: "https://github.com/coenttb/macro-codable-kit.git", branch: "main"),
-        .package(url: "https://github.com/gohanlon/swift-memberwise-init-macro", from: "0.3.0"),
-        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.1.5"),
-        .package(url: "https://github.com/pointfreeco/swift-tagged.git", from: "0.10.0"),
-        .package(url: "https://github.com/pointfreeco/swift-url-routing", from: "0.6.0"),
+        .package(url: "https://github.com/gohanlon/swift-memberwise-init-macro", from: "0.5.1"),
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.6.0"),
     ],
     targets: [
         .target(
             name: .hotjar,
             dependencies: [
+                .coenttbWeb,
                 .memberwiseInit,
-                .swiftHtml
             ]
         ),
         .testTarget(
